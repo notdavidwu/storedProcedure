c46000781f11b021ac56e37dc3901c080833f36f
notdavidwu <davidwu5858@gmail.com>
2023-05-04 10:20:48 +0800

    latest version of expression

--- mark/view_expression.py
+++ mark/view_expression.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.db import connections
 import pyodbc
 import json
-DATABASE_NAME = 'buildVocabulary' 
+DATABASE_NAME = 'nlpVocabularyLatest' 
 
 @csrf_exempt
 def getTag(request):
@@ -40,7 +40,7 @@ def getItemDefinition(request):
 @csrf_exempt
 def getStasticTable(request):
     server = '172.31.6.22' 
-    database = 'buildVocabulary ' 
+    database = 'nlpVocabularyLatest ' 
     username = 'N824' 
     password = 'test81218' 
     conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+'; DATABASE='+database+'; ENCRYPT=yes; UID='+username+'; PWD='+ password +'; TrustServerCertificate=yes;')
@@ -208,7 +208,7 @@ def getStasticTable(request):
 @csrf_exempt
 def getStasticTable2(request):
     server = '172.31.6.22' 
-    database = 'buildVocabulary ' 
+    database = 'nlpVocabularyLatest ' 
     username = 'N824' 
     password = 'test81218' 
     conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+'; DATABASE='+database+'; ENCRYPT=yes; UID='+username+'; PWD='+ password +'; TrustServerCertificate=yes;')
